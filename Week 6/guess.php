5e948bf970df33f679f31bab57ec19c66f042708
Akash Mondal <90198281+spardn@users.noreply.github.com>
2022-10-26 10:32:18 +0530

    Update guess.php

--- Week 6/guess.php
+++ Week 6/guess.php
@@ -1,10 +1,10 @@
 <html>
 <body>
-<title> Anamitra Musib 41884c79 </title>
+<title> Akash Mondal 41884c79 </title>
 <h1> Welcome to my guessing game</h1>
 
 <?php
-$correctnumber=26; //change this number //
+$correctnumber=32; //change this number //
 if(isset($_GET['guess']))
 {
   if (is_numeric($_GET['guess'])===FALSE)
